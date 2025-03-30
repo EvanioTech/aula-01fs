54eff617237cef4e16ce0ae5849a32d35347552b
Seu Nome <seu-email@example.com>
2025-03-30 12:24:36 -0300

    aula reduce

---
+++ src/aulareduce.ts
@@ -0,0 +1,30 @@
+import { Users } from "./aulafind";
+
+ const users = [
+    { id: 1, name: 'Alice', age: 25 },
+    { id: 2, name: 'Bob', age: 30 },
+    { id: 3, name: 'Charlie', age: 35 },
+    { id: 4, name: 'Matias', age: 30, exibir: true }
+]
+
+const reducedUsers = users.reduce((accumulator, user) => {
+    
+    return {
+        ...accumulator,
+        [user.name]: {
+           
+            
+            age: user.age,
+           
+        }
+    };
+}, [] as Users[]);
+const reducedUsers2 = users.reduce((accumulator, user) => {
+    if (user.exibir) {
+        accumulator.push(user);
+    }
+    return accumulator;
+}
+, [] as Users[]);
+console.log(reducedUsers);
+console.log(reducedUsers2);
